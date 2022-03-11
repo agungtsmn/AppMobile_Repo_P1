59fb388fa674602926161292a15065532a590e9b
agungtsmn <0812agung@gmail.com>
2022-03-11 16:21:29 +0700

    Percobaan Variabel Statis dan Dinamis

---
+++ src/main/kotlin/4_Varibel_Statis_dan_Dinamis.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    // Variabel statis bertipe data Integer
+    var statis:Int = 20
+
+    // Variabel dinamis bertipe data String
+    var dinamis = "Agung"
+
+    /**
+     * Variabel statis merupakan variabel yang kita tentukan secara manual tipe datanya
+     * sedangkan variabel dinamis merupakan variabel yang tipe datanya ditentukan secara otomatis dari nilai variabel tersebut
+     */
+
+}
