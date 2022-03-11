b6c66fb8dcaa5d42b919bb79f52595243eedd64b
agungtsmn <0812agung@gmail.com>
2022-03-11 16:32:28 +0700

    Percobaan Tipe Data Number

---
+++ src/main/kotlin/6_Tipe_Data_Number.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    // Tipe data untuk number yang terdapat di dalam kotlin
+    val minByte = -128
+    val maxByte = 128
+    val minShort = -32768
+    val maxShort = 32768
+    val minInt = -2147483648
+    val maxInt = 2147483648
+    val minLong = -9223372036854775807
+    val maxLong = 9223372036854775807
+
+    // Keluaran untuk nilai pada variabel - variabel diatas
+    println("minByte: $minByte")
+    println("maxByte: $maxByte")
+    println("minShort: $minShort")
+    println("maxShort: $maxShort")
+    println("minInt: $minInt")
+    println("maxInt: $maxInt")
+    println("minLong: $minLong")
+    println("maxLong: $maxLong")
+
+}
