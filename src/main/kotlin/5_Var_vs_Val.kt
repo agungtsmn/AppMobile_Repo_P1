230664d0a01fcefe6b4be871e7b0ee25bed608ad
agungtsmn <0812agung@gmail.com>
2022-03-11 16:25:41 +0700

    Percobaan Var vs Val

---
+++ src/main/kotlin/5_Var_vs_Val.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    // Variabel yang nilainya bisa di ubah - ubah (var)
+    var number = 10
+    number = 20
+
+    // Variabel yang nilainya tidak bisa diubah (val)
+    val phi = 3.14
+
+    phi = 2.1 // Error karena variabel tersebut tidak bisa diubah nilainya
+
+}
