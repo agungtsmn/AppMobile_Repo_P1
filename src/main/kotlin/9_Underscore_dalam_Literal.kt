322feaa1c9e570bcbbf636204277e5bfefb56bc3
agungtsmn <0812agung@gmail.com>
2022-03-11 16:50:59 +0700

    Percobaan Underscore dalam Literal

--- src/main/kotlin/9_Underscore_dalam_Literal.kt
+++ src/main/kotlin/9_Underscore_dalam_Literal.kt
@@ -3,6 +3,6 @@ fun main() {
     // Penggunaan underscore di dalam literal
     val oneMillion = 1_000_000
 
-    println("oneMillion = " + oneMillion) // Output = 1000000000
+    println("oneMillion = $oneMillion") // Output = 1000000000
 
 }
