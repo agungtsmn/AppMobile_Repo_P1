e70aee476a5580215d3595fbbf88e627f67da57c
agungtsmn <0812agung@gmail.com>
2022-03-11 16:15:46 +0700

    Percobaan Blok dan Komentar

---
+++ src/main/kotlin/3_Blok_dan_Komentar.kt
@@ -0,0 +1,15 @@
+fun main() { // Blok pembuka
+
+    // Tipe - tipe komentar
+
+    // Komentar satu baris
+
+    /*
+    Komentar multi baris
+     */
+
+    /**
+     * kotlin Doc (Untuk mendokumentasikan kode program)
+     */
+
+} // Blok penutup
