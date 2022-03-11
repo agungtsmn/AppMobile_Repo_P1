e2ad5b8ff67af82dde39147e373748160fdbe29f
agungtsmn <0812agung@gmail.com>
2022-03-11 14:28:15 +0700

    Percobaan Literal

---
+++ src/main/kotlin/1_Literal.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    // deklarasi dan inisialisasi variabel - variabel
+    val intLiteral = 5
+    val doubleLiteral = 5
+    val stringLiteral = "Agung"
+    val charLiteral = '1'
+    val boolLiteral = true
+
+    // menampilkan variabel menggunakan string template
+    println("intLiteral = $intLiteral")
+    println("doubleLiteral = $doubleLiteral")
+    println("stringLiteral = $stringLiteral")
+    println("charLiteral = $charLiteral")
+    println("boolLiteral = $boolLiteral")
+
+}
