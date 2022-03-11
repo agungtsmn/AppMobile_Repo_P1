da0e3cefa73965c125372051f5ba0e77ba70be63
agungtsmn <0812agung@gmail.com>
2022-03-11 16:36:37 +0700

    Percobaan Floating Point

---
+++ src/main/kotlin/7_Floating_Point.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    // Tipe data bilangan desimal (Floating Point) yang terdapat di dalam kotlin
+    val maxFloat: Float = 9.123456789f
+    val maxDouble: Double = 9.123456789
+
+    // Keluaran untuk nilai pada variabel - variabel diatas
+    println("maxFloat:" + maxFloat)
+    println("maxDouble:" + maxDouble)
+
+}
