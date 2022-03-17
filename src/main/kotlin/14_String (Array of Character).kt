664442a398126bbbbb5ebbf7b95ec691a58bdc8f
agungtsmn <0812agung@gmail.com>
2022-03-17 18:41:39 +0700

    Percobaan String (Arrays of Character)

---
+++ src/main/kotlin/14_String (Array of Character).kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    var text = """
+        | tell me and i forget
+        | teach me and i remember
+        | involve me and i learn
+        | - Benjamin Franklin -
+    """.trimIndent()
+
+    println(text)
+
+}
